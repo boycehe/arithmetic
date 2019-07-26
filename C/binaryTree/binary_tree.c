ff26384fb6a34f6a4ab0d5139aaf8df5bb207211
boyce <838304814@qq.com>
2019-07-26 22:31:31 +0800

    #CHG#binary tree

--- C/binaryTree/binary_tree.c
+++ C/binaryTree/binary_tree.c
@@ -7,3 +7,25 @@
 //
 
 #include "binary_tree.h"
+#include <stdlib.h>
+
+binary_tree *initCompleteBinaryTree(int count){
+    
+    binary_tree *tree = (binary_tree *)malloc(sizeof(binary_tree));
+    
+  
+    return tree;
+    
+}
+
+void preOrderBinaryTree(binary_tree *tree){
+    
+}
+
+void intOrderBinaryTree(binary_tree *tree){
+    
+}
+
+void postOrderBinaryTree(binary_tree *tree){
+    
+}
--- C/binaryTree/binary_tree.h
+++ C/binaryTree/binary_tree.h
@@ -11,4 +11,32 @@
 
 #include <stdio.h>
 
+typedef int binary_item_type;
+
+typedef struct __binary_tree{
+    binary_item_type value;
+    struct __binary_tree *parent;
+    struct __binary_tree *leftChild;
+    struct __binary_tree *rightChild;
+    
+}binary_tree;
+/*
+ 初始化完全二叉树
+ count 节点数
+ */
+binary_tree *initCompleteBinaryTree(int count);
+
+/*
+ 前序遍历
+ */
+void preOrderBinaryTree(binary_tree *tree);
+/*
+ 中序遍历
+ */
+void intOrderBinaryTree(binary_tree *tree);
+/*
+ 后序遍历
+ */
+void postOrderBinaryTree(binary_tree *tree);
+
 #endif /* binary_tree_h */
